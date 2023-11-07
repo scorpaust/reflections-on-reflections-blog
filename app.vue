cd33900f116e97c5b116fa63e3fda262b4fc27eb
Dinis <scorpaust@hotmail.com>
2023-11-07 00:13:44 +0000

    SEO Meta

--- app.vue
+++ app.vue
@@ -5,3 +5,17 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'Reflections on Reflections Blog by Evanora Mirrordale',
+  ogTitle: 'Reflections on Reflections Blog by Evanora Mirrordale',
+  description: 'Join Evanora Mirrordale as she explores the unspoken narratives behind our reflections and delves into the art of introspection.',
+  ogDescription: 'Join Evanora Mirrordale as she explores the unspoken narratives behind our reflections and delves into the art of introspection.',
+  ogImage: 'https://a.storyblok.com/f/260985/4743x3162/99951cb3af/pexels-jenna-hamra-804416.jpg',
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Reflections on Reflections Blog by Evanora Mirrordale',
+  twitterDescription: 'Join Evanora Mirrordale as she explores the unspoken narratives behind our reflections.',
+  twitterImage: 'https://a.storyblok.com/f/260985/4743x3162/99951cb3af/pexels-jenna-hamra-804416.jpg'
+})
+</script>
